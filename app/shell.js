67bbc44ffd09c0770424f9b264b102a1a589d8bc
robgallen <robgallen@gmail.com>
2013-09-25 14:53:12 +0100

    Updated hash urls

--- app/shell.js
+++ app/shell.js
@@ -15,14 +15,14 @@
         activate: function () {
             router.map([
                 { route: '', moduleId: 'hello/index', title: 'Hello World' },
-                { route: 'hello*details', hash: '#hello', moduleId: 'hello/index', title: 'Hello World', nav: true },
-                { route: 'view-composition*details', hash:'#view-composition', moduleId: 'viewComposition/index', title: 'View Composition', nav: true },
-                { route: 'modal*details', hash: '#modal', moduleId: 'modal/index', title: 'Modal Dialogs', nav: true },
-                { route: 'event-aggregator*details', hash: '#event-aggregator', moduleId: 'eventAggregator/index', title: 'Events', nav: true },
-                { route: 'widgets*details', hash:'#widgets',  moduleId: 'widgets/index', title: 'Widgets', nav: true },
-                { route: 'master-detail*details', hash: '#master-detail', moduleId: 'masterDetail/index', title: 'Master Detail', nav: true },
-                { route: 'knockout-samples*details', hash: '#knockout-samples', moduleId: 'ko/index', title: 'Knockout Samples', nav: true },
-                { route: 'extras*details', hash: '#extras', moduleId: 'extras/index', title: 'Extras', nav: true }
+                { route: 'hello*details', hash: '#!/hello', moduleId: 'hello/index', title: 'Hello World', nav: true },
+                { route: 'view-composition*details', hash:'#!/view-composition', moduleId: 'viewComposition/index', title: 'View Composition', nav: true },
+                { route: 'modal*details', hash: '#!/modal', moduleId: 'modal/index', title: 'Modal Dialogs', nav: true },
+                { route: 'event-aggregator*details', hash: '#!/event-aggregator', moduleId: 'eventAggregator/index', title: 'Events', nav: true },
+                { route: 'widgets*details', hash:'#!/widgets',  moduleId: 'widgets/index', title: 'Widgets', nav: true },
+                { route: 'master-detail*details', hash: '#!/master-detail', moduleId: 'masterDetail/index', title: 'Master Detail', nav: true },
+                { route: 'knockout-samples*details', hash: '#!/knockout-samples', moduleId: 'ko/index', title: 'Knockout Samples', nav: true },
+                { route: 'extras*details', hash: '#!/extras', moduleId: 'extras/index', title: 'Extras', nav: true }
             ]).buildNavigationModel();
 
             return router.activate();
